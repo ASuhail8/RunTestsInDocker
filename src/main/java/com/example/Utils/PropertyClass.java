96705213b46a339422860ac5ae7bb0d358af97c2
Abdulla Suhail <abdulla.suhail8@gmail.com>
2023-09-02 15:14:07 +0530

    updated pom

--- src/main/java/com/example/Utils/PropertyClass.java
+++ src/main/java/com/example/Utils/PropertyClass.java
@@ -3,19 +3,20 @@ package com.example.Utils;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyClass {
 
     public static String getProperty(String key) {
 
-        FileInputStream fis = null;
+        Properties prop = new Properties();
+        InputStream fis = null;
         try {
             fis = new FileInputStream("src/test/resources/some.properties");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        Properties prop = new Properties();
         try {
             prop.load(fis);
         } catch (IOException e) {
