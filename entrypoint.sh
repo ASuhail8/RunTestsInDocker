af3195ed64939154ef24b9ebb05af91a12c88675
Abdulla Suhail <abdulla.suhail8@gmail.com>
2023-09-09 20:16:09 +0530

    upload s3 from dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run your Selenium tests
-java -DuploadToS3="$UPLOADS3FLAG" -Dbrowser="$BROWSER" -jar my-app-1.0-SNAPSHOT-fat-tests.jar -testjar my-app-1.0-SNAPSHOT-fat-tests.jar -xmlpathinjar "$MODULE"
+java -Dbrowser="$BROWSER" -jar my-app-1.0-SNAPSHOT-fat-tests.jar -testjar my-app-1.0-SNAPSHOT-fat-tests.jar -xmlpathinjar "$MODULE"
 
 # Upload test result files to S3
 aws s3 cp reports s3://my-app-bucket-from-dockerfile/test-results --recursive
